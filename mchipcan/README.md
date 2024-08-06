66e695496f4d096baec1d2b029f133f221c8ab46
mue473 <rainer@mueller-korntal.de>
2024-08-06 19:03:47 +0200

    support also sending CAN frames

---
+++ mchipcan/README.md
@@ -0,0 +1,27 @@
+# mchipcan
+
+Show Live Traffic:
+```
+13:53:17.249  0 0x000B1F18  [5] 00 00 00 4A 01
+13:53:17.250  1 0x00091F18  [6] 00 00 00 4A 00 00
+13:53:17.251  0 0x000D1F18  [6] 00 00 00 4A 00 01
+13:53:35.711  0 0x00301F18  [0]
+13:53:35.954  0 <S>  0x280  [5] <RTR>
+13:53:35.955  1 <S>  0x280  [5] 00 70 00 00 00
+13:53:36.957  0 0x00301F18  [0]
+```
+
+
+Generate different types of CAN-Frames
+```
+rainer@aspire:~$ echo 00a1F18#00000017 > /tmp/cantx 
+rainer@aspire:~$ echo 00a1F18#r4 > /tmp/cantx 
+rainer@aspire:~$ echo 118#00000017 > /tmp/cantx 
+rainer@aspire:~$ echo 118#r4 > /tmp/cantx
+```
+Read MCP2510 internal register 
+(example: 1C = transmit error counter, 1D = receive error counter)
+``` 
+rainer@aspire:~$ echo \#1c > /tmp/cantx 
+rainer@aspire:~$ echo \#1d > /tmp/cantx 
+```
--- mchipcan/mchipcan.c
+++ mchipcan/mchipcan.c
@@ -2,8 +2,12 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <sys/io.h>
+#include <sys/stat.h>
 #include <sys/time.h>
+#include <string.h>
+#include <fcntl.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -11,6 +15,7 @@
 #define INSTRUCTION_WRITE		0x02
 #define INSTRUCTION_READ		0x03
 #define INSTRUCTION_BIT_MODIFY	0x05
+#define INSTRUCTION_RTS(n)	(0x80 | ((n) & 0x07))
 
 /* MPC251x registers */
 #define BFPCTRL		0x0c
@@ -18,6 +23,7 @@
 #define CANCTRL		0x0f
 #define CNF3		0x28
 #define CANINTF		0x2c
+#define TXBCTRL(n)	(((n) * 0x10) + 0x30)
 #define RXBCTRL(n)	(((n) * 0x10) + 0x60)
 
 /* Port support routines */
@@ -27,10 +33,12 @@ void spitransfer(uint8_t *txbuf, uint8_t *rxbuf, int len);
 
 uint8_t txbuf[16];
 uint8_t rxbuf[16] = {0};
+uint8_t txmsg[16];
 
 short debug = 1;
 short port = 0x378;				// LPT1=0x378, LPT2=0x278
 uint8_t opmode = 0 << 5;		// 0=normal, 3=listen_only
+char *txfifo = "/tmp/cantx";	// default trigger
 
 static uint8_t mcp251x_read_reg(uint8_t reg)
 {
@@ -78,6 +86,7 @@ static void decodeMsg(uint8_t * msg, char info)
 		rtr = msg[1] & 0x10;
 		printf("<S>  0x%03X  [%u]", hid >> 5, dlc);
 	}
+	if (info == 'T') rtr = msg[4] & 0x40;
 	if (rtr) printf(" <RTR>");
 	else for (int i=5; i<(dlc + 5); i++) printf(" %02X", msg[i]);
 	printf("\n");
@@ -92,18 +101,59 @@ static void handleRX(int chan)
 	mcp251x_write_bits(CANINTF, 1 << chan, 0);
 }
 
-static void printall()
+static int parseTXcmd(char *line)
 {
-	for (int i=0; i<128; i++) {
-		if ((i % 16) == 0) printf("\n%2X:", i);
-		printf(" %02X", mcp251x_read_reg(i));
+	int dlc = 0;
+	char dby[4];
+	char *sep = strchr(line, '#');
+	if (sep == NULL) return 0;
+	*sep = 0;				// separate
+	int lv = sep - line;
+	if (lv == 0) {			// display MCP251x register
+		long reg = strtol(sep + 1, NULL, 16) & 0xFF;
+		printf("**** register %02lX contains %02X\n", reg, mcp251x_read_reg(reg));
+		return 0;
 	}
-	printf("\n");
+	long id = strtol(line, NULL, 16);
+	if ((lv > 3) || (id > 0x7FF)){
+		if (debug) printf("extended id %08lx ", id);
+		txmsg[2] = (id >> 21) & 0xFF;
+		txmsg[3] = ((id >> 13) & 0xE0) | ((id >> 16) & 3) | 8;
+		txmsg[4] = (id >> 8) & 0xFF;
+		txmsg[5] = id & 0xFF;
+	} else {
+		if (debug) printf("standard id %03lx ", id);
+		txmsg[2] = (id >> 3) & 0xFF;
+		txmsg[3] = (id << 5) & 0xE0;
+	}
+	if ((sep[1] == 'r') || (sep[1] == 'R')) {
+		if ((sep[2] >= '0') && (sep[2] <= '8')) dlc = sep[2] & 0xF;
+		if (debug) printf("RTR %d\n", dlc);
+		txmsg[6] = dlc | 0x40;
+		return 7;
+	}
+	while (sep[1] && sep[2] && (dlc < 8)) {
+		sprintf(dby, "%c%c\n", sep[1], sep[2]);
+		long val = strtol(dby, NULL, 16);
+		txmsg[7 + dlc++] = val & 0xFF;
+		sep += 2;
+	}
+	if (debug) printf("with DLC %d\n", dlc);
+	txmsg[6] = dlc;
+	return (dlc + 7);
 }
 
 int main()
 {
 	uint8_t rdval, status;
+	char *line = NULL;
+	size_t llen;
+	ssize_t nread = -1;
+
+	umask(0);
+	mkfifo(txfifo, 0666);
+	FILE *infifo = fdopen(open(txfifo, O_RDONLY | O_NONBLOCK), "r");
+	if (infifo == NULL) printf("could not open input fifo, no transmission\n");
 
 	if (ioperm(port, 3L, 1)) {
 		printf("error ioperm failed, permission insufficient\n");
@@ -141,7 +191,6 @@ int main()
 		printf("Operation mode not switched correctly, CANSTAT: %2X\n", rdval);
 		return 8;
 	}
-	if (debug) printall();
 
 	// continious CAN handling
 	while (1) {
@@ -150,8 +199,26 @@ int main()
 		if ((status & 0x10) == 0) handleRX(1);
 		else if ((status & 0x70) == 0x30) {			// ints other than RXBF
 			rdval = mcp251x_read_reg(CANINTF);
-			printf("interrupt status is %02X, error flags %02X\n", rdval, rxbuf[3]);
+			if (debug)
+				printf("interrupt status is %02X, error flags %02X\n", rdval, rxbuf[3]);
+			if (rdval & 0x80) printf("**** CAN message error\n");
+			if (rdval & 0x20) printf("**** CAN controller error: %02X\n", rxbuf[3]);
+			if (rdval & 0x1C) decodeMsg(&txmsg[2], 'T');
 			mcp251x_write_bits(CANINTF, rdval & 0xFC, 0);	// reset indications
+		}
+		else if (infifo) {
+			clearerr(infifo);
+			nread = getline(&line, &llen, infifo);	// check if tx requested
+			if (nread > 0) {
+				int tlen = parseTXcmd(line);
+				if (tlen) {
+					txmsg[0] = INSTRUCTION_WRITE;
+					txmsg[1] = TXBCTRL(0) + 1;		// send via TX buffer 0
+					spitransfer(txmsg, NULL, tlen);
+					txmsg[0] = INSTRUCTION_RTS(1);	// trigger transmission 0
+					spitransfer(txmsg, NULL, 1);
+				}
+			}
 		}
 	}
 }
